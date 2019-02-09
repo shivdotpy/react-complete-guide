945a8fd63a315ceee3047266eaa0393eb20f8d69
shiv <shiv@revinfotech.com>
2019-02-09 12:46:12 +0530

    37 state basic

--- src/App.js
+++ src/App.js
@@ -3,17 +3,36 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+
+  state = {
+    persons: [
+      {
+        name: 'shiv',
+        age: 10
+      },
+      {
+        name: 'pilpi',
+        age: 15
+      },
+      {
+        name: 'bavek',
+        age: 20
+      }
+    ]
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I am a react app</h1>
-        <Person name="Max" age="28"></Person>
-        <Person name="shiv" age="10">My Hobbies: Racing</Person>
-        <Person name="Pilpi" age="300"></Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
   }
 }
 
 export default App;
-  
